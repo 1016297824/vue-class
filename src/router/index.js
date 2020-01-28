52d3340be2eadcae44a21498fc385818b2d0ac46
1016297824 <1016297824@qq.com>
2020-01-28 22:13:06 +0800

    2020.01.28

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "@/views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "",
+    redirect: "/home"
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () => import("@/views/About.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: "history"
+});
+
+export default router;
